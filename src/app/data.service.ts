2e9273d407d21e1fe78522f984bddce09d311ff9
gbesset <guillaume.besset.pro@gmail.com>
2018-01-26 11:21:16 +0100

    Mise en place 2 pages, url, service data entre les 2

---
+++ src/app/data.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject'
+
+@Injectable()
+export class DataService {
+
+  private goals = new BehaviorSubject<any>(['My goal #1 ever', 'My second goal']);
+  goal = this.goals.asObservable();
+
+  constructor() { }
+
+  changeGoal(goal){
+  	this.goals.next(goal);
+  }
+
+  testMe(){
+  	return 'ok, service called';
+  }
+}
