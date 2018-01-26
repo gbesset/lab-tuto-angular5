849eeb8feed47768d0a67c62153c0644c7888bcd
gbesset <guillaume.besset.pro@gmail.com>
2018-01-26 15:59:21 +0100

    Ajout service connection http weatherMap et affichage dans chart.js
    Attention, pb de 'Access-Control-Allow-Origin. Plugin chrome a ajouter…

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { DataService } from './data.service';
+import { WeatherService } from './weather.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { MeteoComponent } from './meteo/meteo.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    MeteoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
-  providers: [DataService],
-  bootstrap: [AppComponent]
+  providers: [ 
+        DataService,
+        WeatherService 
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
